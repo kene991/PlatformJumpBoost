ef5cc2216e43ddbf2aefd8f27e6cef2b2fbac45f
kene991 <47773066+kene991@users.noreply.github.com>
2020-09-01 00:40:44 -0400

    Add files via upload

---
+++ Assets/playerJumpBoost.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerJumpBoost : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+     if (collision.gameObject.CompareTag("Green")) //If player jumps on the Green platform they increase the y axis velcity by 4
+        {
+            rb.velocity = Vector2.up * 4;
+        }
+
+     if (collision.gameObject.CompareTag("Yellow")) //If player jumps on the Green platform they increase the y axis velcity by 8
+        {
+            rb.velocity = Vector2.up * 8;
+        }
+
+     if (collision.gameObject.CompareTag("Red")) //If player jumps on the Green platform they increase the y axis velcity by 12
+        {
+            rb.velocity = Vector2.up * 12;
+        }
+    }
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    private Rigidbody2D rb;
+
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float checkedRadius;
+    public LayerMask whatIsGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkedRadius, whatIsGround);
+    }
+
+    //IF PLAYERS PRESS A OR LEFT ARROW, THE MOVE RIGHT! IF PLAYERS PRESS D AND RIGHT ARROW THEY GO LEFT!
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (isGrounded == true) //iF GROUNDED IS EQUAL TO TRUE THEN PLAYERS CAN JUMP!
+        {
+            if ((Input.GetKeyDown(KeyCode.W)) || Input.GetKey(KeyCode.UpArrow))
+            {
+                rb.velocity = Vector2.up * jumpForce;
+            }
+        }
+    }
+}
